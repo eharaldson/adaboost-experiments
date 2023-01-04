2959d6c598dcf439a227a048820ace52d784e88a
Erik Haraldson <erikharaldson@MacBook-Pro.local>
2023-01-04 07:57:37 +0000

    Completed the tuning of hyperparameters for adaboost regression

--- adaboost_regression.py
+++ adaboost_regression.py
@@ -6,14 +6,10 @@ X, y = datasets.load_diabetes(return_X_y=True)
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
 
-ab_model = ensemble.AdaBoostRegressor(estimator=svm.SVR(), n_estimators=100, random_state=0)
+param_grid = {"estimator__degree" : [2, 3, 4, 5, 6, 7],
+              "estimator__kernel" :   ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+              "estimator__nu": [0.3, 0.4, 0.5, 0.6, 0.7],
+              "n_estimators": [50, 100, 250, 500, 1000, 2000]}
 
-ab_model.fit(X_train, y_train)
-
-print(ab_model.score(X_test, y_test))
-
-# param_grid = {"estimator__degree" : [2, 3, 4, 5, 6, 7],
-#               "estimator__kernel" :   ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
-#               "n_estimators": [50, 100, 250, 500, 1000, 2000]}
-
-        
+cv_model = model_selection.GridSearchCV(estimator=ensemble.AdaBoostRegressor(estimator=svm.NuSVR(), random_state=0), n_jobs=-1, verbose=3, param_grid=param_grid)
+cv_model.fit(X_train, y_train)
